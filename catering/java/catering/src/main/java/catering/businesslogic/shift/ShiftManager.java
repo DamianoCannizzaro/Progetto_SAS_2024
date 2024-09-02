d2f951dfc1f3c7b21a4b11c1d404d501b6e75795
DamianoCannizzaro <damiano.cannizzaro@edu.unito.it>
2024-09-02 19:50:23 +0200

    Update on ShiftManager

--- catering/java/catering/src/main/java/catering/businesslogic/shift/ShiftManager.java
+++ catering/java/catering/src/main/java/catering/businesslogic/shift/ShiftManager.java
@@ -1,6 +1,25 @@
 package catering.businesslogic.shift;
 
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class ShiftManager {
-private ShiftTable curentShiftTable;
-private ArrayList<> eventReceivers;
+    private ShiftTable currentShiftTable;
+    private ArrayList<ShiftEventReceiver> eventReceivers;
+
+    public ShiftTable createCookShiftTable(){
+        return st;
+    }
+
+    public void setCurrentShiftTable(ShiftTable currentST) {
+        this.currentShiftTable = currentST;
+    }
+
+    public Shift addShift(ShiftTable st, String type, LocalTime preExt, LocalTime postExt, Date jobDate, Date deadline, LocalTime startTime, LocalTime endTime, boolean gruop, String gruopName){
+        return currentShiftTable.addShift(st, type, preExt, postExt, jobDate, deadline, startTime, endTime, gruop, gruopName );
+    }
+
+
 }
